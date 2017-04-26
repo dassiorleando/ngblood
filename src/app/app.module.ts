b62471fa19a2c5035d7d51fd3cc252dfce6d4c3a
dassiorleando <me@dassiorleando.xyz>
2017-04-26 15:11:58 +0100

    Simple scene layer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,20 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { EsriMapComponent } from './esri-map/esri-map.component';
+import { EsriLoaderService } from 'angular2-esri-loader';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EsriMapComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [EsriLoaderService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
