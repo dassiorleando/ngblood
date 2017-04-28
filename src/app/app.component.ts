76338288a5b35b97a6a44196929492658645e125
dassiorleando <me@dassiorleando.xyz>
2017-04-28 16:49:28 +0100

    Setup two UI for patient, donors and the register modal to share blood

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Share live around the world';
+  title = 'NgBlood';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,23 +3,44 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule }   from '@angular/router';
+
+import { MaterialModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { EsriMapComponent } from './esri-map/esri-map.component';
+import { DonorRegisterDialog, EsriMapDonorComponent } from './esri-map/esri-map-donor.component';
 import { EsriLoaderService } from 'angular2-esri-loader';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import 'hammerjs';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EsriMapComponent
+    EsriMapComponent,
+    EsriMapDonorComponent,
+    DonorRegisterDialog
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MaterialModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot([
+      {
+        path: '', // Patient state
+        component: EsriMapComponent
+      },
+      {
+        path: 'donor',  // Donor state
+        component: EsriMapDonorComponent
+      }
+    ])
   ],
   providers: [EsriLoaderService, EsriMapService],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: [DonorRegisterDialog]
 })
 export class AppModule { }
